669bc4d7da9fa4904bb01e995a226cc04abea095
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2025-04-08 23:37:58 +0530

    Modified: Success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,25 +1,18 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Confetti from "react-confetti";
 
 const SuccessPage = () => {
-  const searchParams = useSearchParams();
   const router = useRouter();
 
-  const orderId = searchParams.get("orderId");
-
   useEffect(() => {
-    if (!orderId) return;
-
     const timer = setTimeout(() => {
-      router.push("/orders/" + orderId);
-    }, 5000);
+      router.push("/orders/1");
+    }, 3000);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [orderId, router]);
+    return () => clearTimeout(timer);
+  }, [router]);
 
   return (
     <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
@@ -28,7 +21,7 @@ const SuccessPage = () => {
       <h2 className="text-xl font-medium">
         We sent the invoice to your e-mail
       </h2>
-      <h3 className="">You are being redirected to the order page...</h3>
+      <h3>You are being redirected to the orders page...</h3>
     </div>
   );
 };
